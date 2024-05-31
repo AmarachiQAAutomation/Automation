d92df2dcac2becbaa409f1e3a875fc73c90ef6fd
AmarachiQAAutomation <achigbuamarachi@yahoo.com>
2024-06-01 00:22:39 +0300

    Add POM tests for issue deletion and deletion cancellation

--- cypress/e2e/e2e-pom/issue-delete-in-pom.cy.js
+++ cypress/e2e/e2e-pom/issue-delete-in-pom.cy.js
@@ -1,25 +1,49 @@
 /**
  * This is an example file and approach for POM in Cypress
  */
-import IssueModal from "../../pages/IssueModal";
+import IssueModal from "../../pages/IssueModal"
+  const issueTitle = 'This is an issue of type: Task.';
 
 describe('Issue delete', () => {
   beforeEach(() => {
     cy.visit('/');
     cy.url().should('eq', `${Cypress.env('baseUrl')}project/board`).then((url) => {
-    //open issue detail modal with title from line 16  
-    cy.contains(issueTitle).click();
-    });
-  });
-
+      cy.contains(issueTitle).click();
+   
+    })
+  })
   //issue title, that we are testing with, saved into variable
   const issueTitle = 'This is an issue of type: Task.';
 
-  it('Should delete issue successfully', () => {
-    //add steps to delete issue
-  });
+ it('Should delete issue successfully', () => {
+  const expectedAmountOfIssuesAfterDeletion = 3;
+      // Click the delete button
+      IssueModal.clickDeleteButton()
 
-  it('Should cancel deletion process successfully', () => {
+       // verify confirmation dialog is visible and confirm delete
+       IssueModal.confirmDeletion()
+  
+       //verify the issuedetial is closed
+      IssueModal.closeDetailModal
+      //verify the number of issue visible on the issue backloglist
+      IssueModal.ensureIssueIsNotVisibleOnBoard(issueTitle);
+       
+ })
+ it('Should cancel deletion process successfully', () => {
+   
+  const EXPECTED_AMOUNT_OF_ISSUES = '4'
+
     //add steps to start deletion proces but cancel it
-  });
-});
+    //verify visbility of  delete button and click
+   IssueModal.clickDeleteButton();
+  //Confirm cancellation
+   IssueModal.cancelDeletion()
+   //confirm issue detail modal is closed
+   IssueModal.closeDetailModal()
+   //confirm that the issue is visible on the Board
+   IssueModal.ensureIssueIsVisibleOnBoard(issueTitle)
+   
+ })
+
+
+})
